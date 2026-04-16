d30d4f82cded0f01e9b0f34be526de193122de73
Allan B. <26cleme@mail.apollocc.org>
2026-04-16 11:52:42 -0400

    Cameras

--- Scripts/game.gd
+++ Scripts/game.gd
@@ -5,6 +5,8 @@ extends Node2D
 
 var ROOM_BANK : Array
 var CAMERA_BANK : Array
+var CAMERA_AREA_BANK : Array
+# --- ON READY ---
 func _ready():
 	var SCENE = $"Apartment Level"
 	for i in SCENE.get_children():
@@ -14,14 +16,59 @@ func _ready():
 		for x in i.get_children():
 			if x is Camera2D:
 				CAMERA_BANK.append(x)
-	print(ROOM_BANK)
-	print(CAMERA_BANK)
 	
-	for camera in Camera
+	# - Give each camera an area -
+	for i : Camera2D in CAMERA_BANK:
+		# Create nodes for this camera
+		## Create area node for camera
+		var camera_area = Area2D.new()
+		### Name camera area
+		var room = i.get_parent()
+		camera_area.name = room.name + " Camera Area"
+		## Create rectangle collision node for camera's area node
+		var camera_area_collision = CollisionShape2D.new()
+		
+		# Move those nodes as children of this camera
+		## Move collision node as child of area
+		camera_area.add_child(camera_area_collision)
+		## Move area node as child of camera
+		i.add_child(camera_area)
+		
+		# Change area's collision node to size as camera size
+		## Camera width and height values
+		var camera_width : int = abs(i.limit_right - i.limit_left)
+		var camera_height : int = abs(i.limit_bottom - i.limit_top)
+		## Create collision rectangle shape
+		var area_collision_shape = RectangleShape2D.new()
+		## Define size as camera size
+		area_collision_shape.size = Vector2(camera_width,camera_height)
+		## Apply rectangle shape as area collision's shape
+		camera_area_collision.shape = area_collision_shape
+		# Change area's collision node to position at camera
+		## Tiles are even, so cameras will always be even
+		@warning_ignore("integer_division")
+		camera_area_collision.transform.origin = Vector2(camera_width/2,camera_height/2)
+		
+		# Append area to array
+		CAMERA_AREA_BANK.append(camera_area)
+	
+	# Camera On Start
+	current_camera = CAMERA_BANK[0]
 
-
-
+# --- LOOP ---
+## The current camera
+var current_camera : Camera2D
 func _process(_delta):
-	for camera in CAMERA_BANK:
-		pass
+	# Find which camera has player in area
+	for i : Area2D in CAMERA_AREA_BANK:
+		# Detect bodies in camera's area
+		for x in i.get_overlapping_bodies():
+			# If body is the player body
+			if x == PLAYER:
+				current_camera = i.get_parent()
+	
+	
+	
+	# Swap cameras
+	current_camera.make_current()
 	
